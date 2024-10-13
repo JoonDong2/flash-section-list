1e695cbad02c153f8c7f8ed55e18822cece17930
Joondong <kwonjoondong@gmail.com>
2024-10-13 18:41:34 +0900

    feat: add flash-list

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,3 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
-}
+import { FlashList } from '@shopify/flash-list';
+
+export const FlashSectionList = FlashList;
